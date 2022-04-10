ba5b754431269b5eb4dad3100717c586a6cd544e
Corey Damocles <damoclescj@gmail.com>
2022-04-10 16:45:07 -0700

    add index.html, normalize.css, reset.css, style.css, and updated CodwarsPractice.js

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -40,3 +40,86 @@
 //function toFreud(string){
 //  return String.replace(/[^\s]+/g, "sex");
 //}
+//
+//
+//Create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+// function removeChar(str){
+//     if (str.length < 2){
+//         return str;
+//     }else{
+//     return str.slice(1,str.length-1)
+//     }
+// }
+
+// There are many ways to browse an array element by element.
+// One way is to use a for loop. Think up a for loop to iterate a given array:
+// const movies = ["the wolf of wall street", "Zootopia", "babysitting"];
+// for (i=1; i < movies.length; i++){
+//     console.log(movies[i]);
+// }
+//Now think up of a way to iterate (create and show) the following given array using forEach():
+//movies.forEach(element => {
+//     console.log(element);
+// });
+
+
+
+//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+// function multiplyAndAlertProduct(arr){
+//     let product = 1;
+//     arr.forEach(element => product *= element);
+//     alert (product);
+// }
+
+
+// Write a function which calculates the average of the numbers in a given list:
+// Note: Empty arrays should return 0.
+
+// function find_average(arr) {
+//     let result = 0;
+//     if (arr.length === 0) {
+//     return 0;
+//     }else{
+//     arr.forEach (element => result += element);
+//     console.log(result / arr.length);
+//     }
+//     }
+
+
+//Complete the solution so that it reverses the string passed into it.
+// function solution(str){
+//     return str.split("").reverse().join("");
+//   }
+
+// Arrow SyntaxError
+// const solution = str => str.split("").reverse().join("");
+
+var isAnagram = function(test, original) {
+    let test1 = test.split("").sort();
+    let original1 = original.split("").sort();
+    if (test1.toLowerCase.toString() === original1.toLowerCase.toString()){
+        console.log("true");
+        return true;
+    }else{
+        console.log("false");
+        console.log(test1, original1);
+        console.log(test, original);
+        return false;
+    }
+};
+    
+//     if (JSON.stringify(test1).toLowerCase === JSON.stringify(original1).toLowerCase){
+//         console.log("true");
+//         console.log(JSON.stringify(test1));
+//         console.log(JSON.stringify(original1));
+//         return true;
+//     }else{
+//         console.log("false");
+//         console.log(test1, original1);
+//         console.log(test, original);
+//         return false;
+//     }
+// };
+
+isAnagram("dumble", "bumble");
