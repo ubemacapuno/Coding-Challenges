c676761f8e5d3bcceb752184ec50a67173f1c1ef
Corey Damocles <damoclescj@gmail.com>
2022-04-18 21:43:14 -0700

    adding changes

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -120,3 +120,19 @@
 //       return 0
 //     }
 //   }
+
+//Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.(FIRST TRY!! 4.18.22)
+// function getGrade (s1, s2, s3) {
+//     let score = ((s1 + s2 + s3)/ 3)
+//     if(score >= 90 && score <= 100){
+//         return 'A'
+//     }else if(score >= 80 && score < 90){
+//         return 'B'
+//     }else if(score >=70 && score < 80){
+//         return 'C'
+//     }else if(score >=60 && score <70){
+//         return 'D'
+//     }else if(score < 60){
+//         return 'F'
+//     }
+//   }
