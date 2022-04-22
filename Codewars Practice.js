e03d70b9a3e723e2a6a00e7a47ae2904ba888656
Corey Damocles <damoclescj@gmail.com>
2022-04-21 23:04:56 -0700

    add additional problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -364,3 +364,25 @@
 //   }
 //Other solution:
 // const cockroachSpeed = s => Math.floor(s / 0.036);
+
+// Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+// Examples:
+
+// setAlarm(true, true) -> false
+// setAlarm(false, true) -> false
+// setAlarm(false, false) -> false
+// setAlarm(true, false) -> true
+
+// my code:
+// function setAlarm(employed, vacation){
+//     if ((employed === true && vacation === true)||(employed === false && vacation === false)||(employed === false && vacation === true)){
+//       return false
+//     }else{
+//       return true
+//     }
+//   }
+
+// Other solution:
+// function setAlarm(employed, vacation){
+//     return employed && !vacation;
+//   }
