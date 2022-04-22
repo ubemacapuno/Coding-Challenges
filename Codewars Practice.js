f6972be410a1a48fe37e9a08838dbd3010848025
Corey Damocles <damoclescj@gmail.com>
2022-04-21 22:55:56 -0700

    adding prob & solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -357,3 +357,10 @@
 
 // const minMax = arr => [ Math.min(...arr), Math.max(...arr) ];
 
+//The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+//function cockroachSpeed(s) {
+//     let cmPerSecond = Math.floor((s * (100000/3600)))
+//     return cmPerSecond
+//   }
+//Other solution:
+// const cockroachSpeed = s => Math.floor(s / 0.036);
