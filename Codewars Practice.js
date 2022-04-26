07a0be4bd66b5a3035248930aa5eb2606322a032
Corey Damocles <damoclescj@gmail.com>
2022-04-26 09:59:43 -0700

    Added 7Kyu problem and solutions

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -468,4 +468,32 @@
 //     const filteredArray = l.filter(element => typeof element === 'number')
 //     return filteredArray
 //   }
+//
+//Kyu 7
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+// My Solution:
+// function highAndLow(numbers){
+//     numbers = numbers.split(' ').sort((a, b) => a - b)
+//     console.log(numbers)
+//     if(numbers.length == 1){
+//         console.log(`${numbers.toString()} ${numbers.toString()}`)
+//         return `${numbers.toString()} ${numbers.toString()}`
+//     }else{
+//     let numbersArr = [numbers.pop(), numbers.shift()]
+//     console.log(numbersArr);
+//     console.log(numbersArr.toString().replace(/,/g,' '))
+//     return numbersArr.toString().replace(/,/g,' ')
+//     }  
+//   }
+//
+//   Test Cases:
+//   highAndLow("42"); //return 42 42
+//   highAndLow("1 9 3 4 -5"); //return 9 -5
+//   highAndLow("1 2 3"); //return 3 1
+//
+//BETTER Solution:
+// function highAndLow(numbers){
+//     numbers = numbers.split(' ');
+//     return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+//   }
 
