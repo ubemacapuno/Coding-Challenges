9d9e3eb2fac4e9fd3ca5b8aafe1f5fccd5c8b36a
Corey Damocles <damoclescj@gmail.com>
2022-04-29 09:37:37 -0700

    Adding coding problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -593,3 +593,20 @@
 // Make a simple function called greet that returns the most-famous "hello world!".
 //Arrow fxn:
 // let greet = () => "hello world!"
+
+// We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+// You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+// All values will be positive integers or floats, or zero.
+
+// My solutions (2)
+//Arrow function:
+// const plural = (n) => n < 1 || n > 1? true : false;
+//function declaration:
+
+// function plural(n) {
+//   if(n < 1 || n > 1){
+//     return true;
+//   }else{
+//     return false;
+//   }
+//   }
