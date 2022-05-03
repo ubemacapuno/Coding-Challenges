63b2aca0e1de000fb8f61f402ca8e0a9b3fc5733
Corey Damocles <damoclescj@gmail.com>
2022-05-02 21:23:01 -0700

    Adding codewars problems and solutions

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -696,5 +696,12 @@
 //         return "error sum of facing turn"
 //     }
 // }
-
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+// Function Expression:
+// function grow(x){
+//   return x.reduce((acc,c)=> acc*c,1)
+//   }
+// Arrow Function
+// const grow = (x) => x.reduce((acc,c)=> acc*c,1)
 
