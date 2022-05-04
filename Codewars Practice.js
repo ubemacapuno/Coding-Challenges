a7fa64920ca4bb7d9417b20e53dcf09353595ffa
Corey Damocles <damoclescj@gmail.com>
2022-05-04 16:59:37 -0700

    Added codewars problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -738,3 +738,15 @@
 // function nameShuffler(str){
 //     return str.split(' ').reverse().toString().replace(/,/g, ' ')
 //   }
+//Sentence Smash
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+//Function Declaration
+// function smash (words) {
+//     return words.join(' ')
+//  };
+
+ //Arrow Function
+//  const smash = (words) => words.join(' ')
+//Test cases:
+//  smash(["this", "is", "a", "really", "long", "sentence"])
+//  smash(["hello", "amazing", "world"])
