e5c1463f4d1c2e017233fca643331283a2deddc2
Corey Damocles <damoclescj@gmail.com>
2022-05-04 16:50:30 -0700

    Added problems and solutions (codewars 8kyu)

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -724,3 +724,17 @@
 //         return "Shark Bait!"
 //     }
 // }
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// nameShuffler('john McClane'); => "McClane john"
+//My Solution:
+// function nameShuffler(str){
+//     return str.split(' ').reverse().toString().replace(/,/g, ' ')
+//   }
+
+//   nameShuffler("Corey Damocles")
+//Top Solution:
+// function nameShuffler(str){
+//     return str.split(' ').reverse().toString().replace(/,/g, ' ')
+//   }
