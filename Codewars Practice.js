1c7e3005285b04785895953700cb6fc8e586a89f
Corey Damocles <damoclescj@gmail.com>
2022-05-07 12:36:02 -0700

    Adding codewars problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -803,3 +803,21 @@
 // return uniqueChars
 // }
 
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+// Array can contain numbers or strings. X can be either.
+// Return true if the array contains the value, false if not.
+
+// function check(a, x) {
+//     if (a.includes(x)){
+//         console.log('true')
+//       return true
+//     }else{
+//         console.log('false')
+//     return false
+//     }
+//   }
+
+
+  let check = (a,x) => a.includes(x)? true : false
+
+  check([66, 101], 66)
