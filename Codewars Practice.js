ea31bb992ee83bfa10e973262cf60e3a3b0b7296
Corey Damocles <damoclescj@gmail.com>
2022-05-09 21:58:16 -0700

    Adding codewars problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -841,3 +841,23 @@
 //   }
 //Arrow Function:
 // const litres = time => Math.floor(time * 0.5)
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+// It should look like this:
+// Sam Harris => S.H
+// patrick feeney => P.F
+
+// My Solution:
+// function abbrevName(name){
+//   let firstName = name.split(" ").shift()
+//   let secondName = name.split(" ").pop()
+//   console.log(`${firstName.toUpperCase().charAt(0)}.${secondName.toUpperCase().charAt(0)}`)
+// }
+
+//Test Cases:
+// abbrevName('Sam Harris')
+// abbrevName('patrick feeney')
+
+//Codewars Arrow Function solution:
+// const abbrevName = (name) => name.split(' ').map(i => i.charAt(0).toUpperCase()).join('.');
