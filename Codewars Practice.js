ea4476b9594ed3b2875a40fb1556779334c604ab
Corey Damocles <damoclescj@gmail.com>
2022-05-10 19:19:47 -0700

    Adding code wars problem and solution 6KYU solved first trygit add .

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -882,3 +882,36 @@
 //   }
 //Ternary Operator + arrow function solution:
 // const greet = name => name === "Johnny"? "Hello, my love!" : `Hello, ${name}!`
+
+
+//6Kyu first Tryyyyy 05/10/2022
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+//My solution (FIRST TRY!!)
+//
+// function likes(names) {
+//     if(names.length === 0){
+//         return "no one likes this"
+//     }else if(names.length === 1){
+//         return `${names} likes this`
+//     }else if(names.length === 2){
+//         return `${names[0]} and ${names[1]} like this`
+//     }else if(names.length === 3){
+//         return `${names[0]}, ${names[1]} and ${names[2]} like this`
+//     }else{
+//         return `${names[0]}, ${names[1]} and ${names.length -2} others like this`
+//     }
+// }
+//
+// test cases:
+// console.log(likes([]))
+// console.log(likes(["Peter"]))
+// console.log(likes(["Jacob", "Alex"]  ))
+// console.log(likes(["Max", "John", "Mark"] ))
+// console.log(likes(["Alex", "Jacob", "Mark", "Max"] ))
+
