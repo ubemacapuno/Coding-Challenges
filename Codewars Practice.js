a1e5f8d9c63bde19be9a970123ed67beef652423
Corey Damocles <damoclescj@gmail.com>
2022-05-12 19:49:22 -0700

    Added another codewars problem and solution 5.12.22

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -933,10 +933,12 @@
 
 // Complete the solution so that it reverses all of the words within the string passed in.
 // Example: "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
-
-function reverseWords(str){
-    return str.split(" ").reverse().join(' ')
-  }
-
-  console.log(reverseWords("The greatest victory is that which requires no battle"))
-
+//My solutions:
+//Function declaration:
+// function reverseWords(str){
+//     return str.split(" ").reverse().join(' ')
+//   }
+//Arrow function:
+// const reverseWords = s => s.split(" ").reverse().join(' ')
+// Test case:
+// console.log(reverseWords("The greatest victory is that which requires no battle"))
