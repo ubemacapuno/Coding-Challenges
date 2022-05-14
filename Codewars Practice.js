ccc20f6f598ea06f0beba901aad8ee60ed312eab
Corey Damocles <damoclescj@gmail.com>
2022-05-14 11:25:03 -0700

    adding codewars problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1037,3 +1037,17 @@
 // console.log(getMiddle("fourteen"))
 // console.log(getMiddle("CJ"))
 // console.log(getMiddle("J"))
+
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+// For example, update_light('green') should return 'yellow'.
+//My solution:
+// function updateLight(current) {
+//     if(current == 'green'){
+//       return current = 'yellow'
+//     }else if(current == 'yellow'){
+//       return current = 'red'
+//     }else{
+//       return current = 'green'
+//     }
+//   }
