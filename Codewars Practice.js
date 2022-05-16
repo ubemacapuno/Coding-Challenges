f6e726a1565c0f621fcec3daabf857b080f9719e
Corey Damocles <damoclescj@gmail.com>
2022-05-16 16:37:45 -0700

    Added codewars problem and solution 8Kyu

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1112,9 +1112,11 @@
 // You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
 // The returned value must be a string, and have "***" between each of its letters.
 // You should not remove or add elements from/to the array.
-
-function twoSort(s) {
-    return s.shift().split('').join('***').toString();
-  }
-
-  console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
+// My solution:
+// function twoSort(s) {
+//     return s.sort().shift().split('').join('***').toString();
+//   }
+//Arrow function solution:
+// const twoSort = s => s.sort().shift().split('').join('***').toString();
+//Test Cases:
+// console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
