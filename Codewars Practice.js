8b6ec2a1740c0ef8ff1c39034b5c3dba7910d38a
Corey Damocles <damoclescj@gmail.com>
2022-05-15 20:04:03 -0700

    Adding 7Kyu problem and solution 5/15/22

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1075,3 +1075,19 @@
 //Test Cases:
 // console.log(warnTheSheep(["sheep","wolf","sheep", "sheep", "sheep"]))
 // console.log(warnTheSheep(["sheep", "sheep", "wolf"]))
+
+//7Kyu 05/15/2022
+// Return the number (count) of vowels in the given string.
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+// The input string will only consist of lower case letters and/or spaces.
+//My solution:
+// function getCount(str) {
+//     let vowelsCount = 0;
+//     let arr = str.split('')
+//     for(i=0;i<arr.length;i++){
+//         if(arr[i]=="a" || arr[i]=="e" || arr[i]=="i" || arr[i]=="o" || arr[i]=="u"){
+//             vowelsCount++
+//         }
+//     }
+//     return vowelsCount;
+//   }
