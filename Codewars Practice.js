186097deb429e7d872fb0cb3d3d48561510c15be
Corey Damocles <damoclescj@gmail.com>
2022-05-18 18:02:49 -0700

    Added 7Kyu problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1190,3 +1190,21 @@
 // console.log(sum([-2.398]))
 //My arrow function solution
 // const sum = (numbers) => numbers == []? 0:numbers.reduce((a,c)=>a+c,0)
+
+// 7Kyu
+// This time no story, no theory. The examples below show you how to write function accum:
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+//my solution:
+// function accum(s) {
+//     let c = s.toLowerCase().split("").map((e,i)=>e.repeat(i+1))
+//     for(i=0;i<c.length;i++){
+//         c[i] = c[i].charAt(0).toUpperCase() + c[i].slice(1);
+//     }
+//     return c.join("-")
+// }
+// test case:
+// console.log(accum("idGaF"))
