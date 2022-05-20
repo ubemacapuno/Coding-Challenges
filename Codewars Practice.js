485a56ac7869f067ef2c0f34fdfa6cde086cafae
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-05-20 11:34:22 -0700

    Added codewars 8kyu problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1248,3 +1248,18 @@
 // const sumMix = x => x.map(e=>e*1).reduce((a, c) =>a + c,0);
 //Test Cases:
 // console.log(sumMix([1,2,3,"4"]));
+
+// 8 kyu Century From Year
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+//my solution: 
+//const century = y => Math.ceil(y/100)
