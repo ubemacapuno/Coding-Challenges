1f49ed48632a3d5a43b3e95873fdaf2428aa63a8
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-05-19 18:23:30 -0700

    Added codewars problem and solution 8Kyu

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1208,3 +1208,30 @@
 // }
 // test case:
 // console.log(accum("idGaF"))
+
+//8 kyu Beginner Series #2 Clock 5.19.2022 (First try!!)
+// Clock shows h hours, m minutes and s seconds after midnight.
+// Your task is to write a function which returns the time since midnight in milliseconds.
+// Example:
+// h = 0
+// m = 1
+// s = 1
+// result = 61000
+// Input constraints:
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+//My Solutiion:
+// function past(h, m, s){
+//     //#Happy Coding! ^_^
+//     let hours = (0 + h) * 3.6e+6;
+//     let minutes = (0 + m) * 60000;
+//     let seconds = (0 + s) * 1000;
+//     return hours + minutes + seconds
+//   }
+//Test Case:
+// console.log(past(0,1,1))
+//Better solution:
+// function past(h, m, s){
+//     return ((h*3600)+(m*60)+s)*1000;
+//   }
