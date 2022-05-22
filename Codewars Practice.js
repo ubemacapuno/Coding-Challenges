9be316bc5d2f22316b36e91aa7c9fbc360cdb5d2
Corey Damocles <damoclescj@gmail.com>
2022-05-21 22:16:06 -0700

    Added codewars problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1316,3 +1316,21 @@
 //     return value1 / value2
 //   }
 // }
+//
+// 7 kyu Reverse words
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+//My solution:
+// function reverseWords(str) {
+//     return str.split(' ').map((e)=>e.split('').reverse()).join(' ').replace(/,/g, '')
+// }
+//Arrow Function:
+// const reverseWords = s => s.split(' ').map((e)=>e.split('').reverse()).join(' ').replace(/,/g, '')
+//BETTER solution without dreaded regex:
+// const reverseWords = s => s.split(' ').map((e)=>e.split('').reverse().join('')).join(' ')
+//Test Cases:
+// console.log(reverseWords("This is an example!"))
+// console.log(reverseWords("double  spaces"))
