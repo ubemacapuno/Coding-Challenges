b963e0b49b45fc6c79f88386c5c9b9551c7f1b7c
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-05-22 10:45:17 -0700

    Added 7Kyu problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1334,3 +1334,32 @@
 //Test Cases:
 // console.log(reverseWords("This is an example!"))
 // console.log(reverseWords("double  spaces"))
+
+//7 kyu Complementary DNA 05.22.2022
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+// Example: (input --> output)
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+// dnaStrand []        `shouldBe` []
+// dnaStrand [A,T,G,C] `shouldBe` [T,A,C,G]
+// dnaStrand [G,T,A,T] `shouldBe` [C,A,T,A]
+// dnaStrand [A,A,A,A] `shouldBe` [T,T,T,T]
+//My solution! Run "If-Else" within an array.map!! FIRST TRY
+// function DNAStrand(dna){
+//     let dnaArray = dna.split('').map(e=>
+//     {if(e==="A"){
+//         return "T"
+//     }else if (e==="T"){
+//         return "A"
+//     }else if (e==="G"){
+//         return "C"
+//     }else{
+//         return "G"
+//     }})
+//     return dnaArray.join('')
+//   }
+// Test Case:
+// console.log(DNAStrand("ATTGC"))
