ba6ead00f1a8d47daf837a532b4734d290a2da56
Corey Damocles <damoclescj@gmail.com>
2022-05-24 08:28:07 -0700

    Added codewars problem and solution - Hello World variation

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1372,3 +1372,15 @@
 // n=-5, m=5:  0
 //My solution:
 // const paperwork = (n,m) => n < 0 || m < 0? 0 : n*m
+
+//8 kyu 05.24.2022 Training JS #1: create your first JS function and print "Hello World!"
+// Task
+// Please refer to two example above and write your first JS function.
+// mission 1:
+// use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+// mission 2:
+// use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+// mission 3:
+// type the console.log() in the next line(don't forget to put the str in the brackets).
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+// In the end, click "Submit" to submit your code pass this kata.
