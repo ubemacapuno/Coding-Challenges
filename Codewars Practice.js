eca2748865535cf625d583cb259ac3b32791ea69
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-05-28 09:42:37 -0700

    Added 7Kyu array problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1535,3 +1535,19 @@ getFetch()
     // fill with 0 from position 2 until position 4
 // console.log(array1.fill(0, 2, 4));
     // expected output: [1, 2, 0, 0]
+
+// 7 kyu Even numbers in an array 05/28/2022
+// Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+// For example:
+// ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+// ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+// ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+//My solution:
+// function evenNumbers(array, number) {
+//     let filteredArray = array.filter (e=> e % 2 === 0)
+//     return filteredArray.slice(filteredArray.length-number)
+//   }
+//Other solution:
+// const evenNumbers = (array, number) => array.filter(e=> e % 2 === 0).slice(-number)
+//Test
+// console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))
