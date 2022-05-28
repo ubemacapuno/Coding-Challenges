999ecc24e817a59a006f9169991b57bb4f601f79
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-05-27 20:33:29 -0700

    Added 7Kyu and 6Kyu array problems and solutions

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1492,3 +1492,46 @@ getFetch()
 //Test case:
 // console.log(sortByLength(["Beg", "Life", "I", "To"]))
 
+//7 kyu JavaScript Array Filter
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+// The solution would work like the following:
+// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+//My solution:
+// function getEvenNumbers(numbersArray){
+//   return numbersArray.filter(e=>e % 2 === 0)
+// }
+//Arrow syntax:
+// let getEvenNumbers = numbersArray => numbersArray.filter(e=> e % 2 === 0)
+// console.log(getEvenNumbers([2,4,5,6]))
+
+// 6 kyu Pyramid Array
+// Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+// pyramid(0) => [ ]
+// pyramid(1) => [ [1] ]
+// pyramid(2) => [ [1], [1, 1] ]
+// pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+// Note: the subarrays should be filled with 1s
+//p: number >= 0
+//r: Array of ascending length SUBARRAYS
+//e: Pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+//p: Map the array? Loop? 
+//solution:
+// function pyramid(n) {
+//   let arrayOfN = []
+//   for(i=0;i<n;i++){
+//     arrayOfN.push([...Array(i+1)].fill(1))
+//   }
+//   return arrayOfN
+// }
+//Other solution:
+// const pyramid = n => Array(n).fill(1).map((x, i) => Array(i + 1).fill(1))
+//Test case:
+// console.log(pyramid(5))
+//ANKI this:
+//Array() Array constructor is used to create Array objects
+//Example: [element0, element1, /* ... ,*/ elementN]
+//array.fill() method changes ALL elements in an array to a STATIC VALUE (in this problem, we want a static number 1)
+//example: 
+    // fill with 0 from position 2 until position 4
+// console.log(array1.fill(0, 2, 4));
+    // expected output: [1, 2, 0, 0]
