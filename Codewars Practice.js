43225dea3994eeb84fcba40fabfe8158afe7e551
Corey Damocles <damoclescj@gmail.com>
2022-05-30 11:53:33 -0700

    Added 8Kyu problem

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1665,3 +1665,18 @@ getFetch()
 // Note: Only valid inputs will be given.
 //My solution: 
 // const booleanToString = b => b === true ? "true" : "false"
+
+// Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+// Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+// Examples:
+// * With `name` = "john"  => return "Hello, John!"
+// * With `name` = "aliCE" => return "Hello, Alice!"
+// * With `name` not given 
+//   or `name` = ""        => return "Hello, World!"
+
+// function hello(name) {
+//     return name ? `Hello, ${name.toLowerCase().split("").shift().toString().toUpperCase() + name.toLowerCase().split("").splice(1).join("").toString()}!` : "Hello, World!" 
+//  }
+//Test Cases:
+// console.log(hello("aliCE"))
+// console.log(hello())
