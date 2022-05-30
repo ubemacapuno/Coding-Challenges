4fbf28fa956c57da72f1c87aa2b8fb81abd59d3d
Corey Damocles <damoclescj@gmail.com>
2022-05-30 10:21:31 -0700

    Added 8Kyu string problem01~

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1659,3 +1659,9 @@ getFetch()
 //     return (arrayN===n ? true : false)
 // }
 //Test case: console.log(isNarcissistic(154))
+
+//8 kyu Convert a Boolean to a String
+// Implement a function which convert the given boolean value into its string representation.
+// Note: Only valid inputs will be given.
+//My solution: 
+// const booleanToString = b => b === true ? "true" : "false"
