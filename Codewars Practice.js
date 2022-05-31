a13e9a8fa213e5c6bdf5e161da53c8a36701f95e
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-05-31 10:25:30 -0700

    Added 8Kyu problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1683,3 +1683,18 @@ getFetch()
 // console.log(disemvowel('This website is for losers LOL!'))
 // console.log(disemvowel("No offense but,\nYour writing is among the worst I've ever read"))
 // console.log(disemvowel("What are you, a communist?"))
+
+// 8 kyu A Needle in the Haystack
+// Can you find the needle in the haystack?
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+// After your function finds the needle it should return a message (as a string) that says:
+// "found the needle at position " plus the index it found the needle, so:
+// findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+//My solution:
+// function findNeedle(haystack) {
+//     return `found the needle at position ${haystack.indexOf('needle')}`
+//   }
+//Arrow syntax:
+// const findNeedle = haystack => `found the needle at position ${haystack.indexOf('needle')}`
+//Test cases:
+// console.log(findNeedle['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false])
