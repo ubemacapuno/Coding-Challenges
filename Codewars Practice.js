07d55a85b7441fb13bd53965bf64fcd7d901494b
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-06-05 10:46:04 -0700

    Added 7Kyu problem and solutions

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -1741,20 +1741,20 @@ getFetch()
             //return false if duplicates are found
     //If the array passes the loop, the code proceeds and true is returned.
 
-function isIsogram(str){
-    if(str.isEmpty){
-        return true; // .isEmpty() method checks whether string is empty or not
-    }else{
-        str = str.toLowerCase() // return is not case-sensitive
-    }
-    let sortedArr = str.split('').sort()
-    for(i=0;i<sortedArr.length;i++){
-        if (sortedArr[i + 1] == sortedArr[i]) {
-            return false //the array is sorted, so if any duplicates are found during iteration, false is returned
-        }
-    }
-    return true
-}
+// function isIsogram(str){
+//     if(str.isEmpty){
+//         return true; // .isEmpty() method checks whether string is empty or not
+    // }else{
+    //     str = str.toLowerCase() // return is not case-sensitive
+    // }
+    // let sortedArr = str.split('').sort()
+    // for(i=0;i<sortedArr.length;i++){
+    //     if (sortedArr[i + 1] == sortedArr[i]) {
+    //         return false //the array is sorted, so if any duplicates are found during iteration, false is returned
+//         }
+//     }
+//     return true
+// }
 // Shorter but similar solution from other user:
 // function isIsogram(word){
 //     word = word.toLowerCase().split("").sort();
@@ -1767,3 +1767,25 @@ function isIsogram(str){
 //Test Cases:
 // console.log(isIsogram("Dermatoglyphics"))
 // console.log(isIsogram(""))
+
+//7 kyu Shortest Word 06/05/2022
+// Simple, given a string of words, return the length of the shortest word(s).
+// String will never be empty and you do not need to account for different data types.
+//P: String of words
+//R: number (length of shortest word(s))
+//E: "bitcoin take over the world maybe who knows perhaps" //3
+//Psuedocode: 
+    //split words into an array of words (.split(' '))
+    //map the array and return the length of each element (.map(e=>e.length))
+    //sort the array from  smallest to largest (.sort((a,b)=>a-b)
+    //return the element at index[0], which should be the smallest word length (.shift())
+//My solution:
+function findShort(s){
+    return s.split(' ').map(e=>e.length).sort((a,b)=>a-b).shift()
+}
+//refactored:
+const findShort = s => s.split(' ').map(e=>e.length).sort((a,b)=>a-b).shift()
+//Similar solution from other Codewars user; Math.min(...s) was used instead:
+const findShort = s => Math.min(...s.split(" ").map(a => a.length))
+// Test case:
+// console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
