ffec98c9cdfe927a0ec2f2c8902b88534ae90921
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-07-17 10:33:35 -0700

    Added 8Kyu

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -3687,3 +3687,23 @@ getFetch()
 //   }
 // //Test Case:
 // console.log(sortArray([5, 8, 6, 3, 4]))
+
+// 8 kyu Exclusive "or" (xor) Logical Operator
+// Overview
+// In some scripting languages like PHP, there exists a logical operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or" (hence the name of this Kata). The exclusive or evaluates two booleans. It then returns true if exactly one of the two expressions are true, false otherwise. For example:
+// false xor false == false // since both are false
+// true xor false == true // exactly one of the two expressions are true
+// false xor true == true // exactly one of the two expressions are true
+// true xor true == false // Both are true.  "xor" only returns true if EXACTLY one of the two expressions evaluate to true.
+// Task
+// Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
+//P: a and b, two expressions to be evaluated
+//R: True if exactly one of the two expressions evaluate to true, false otherwise
+//E: false, false //false (see above examples)
+//Pseudocode:
+    //evaluate if a == b. Return false, otherwise return true
+//my solution:
+// const xor = (a,b) => a == b ? false : true
+// //Refactored:
+// const xor = (a,b) => a != b
+
