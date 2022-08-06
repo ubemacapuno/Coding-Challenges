828b980ee620976b31dff551b87768634e92382c
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-08-06 09:36:29 -0700

    Updated comments for findTheStrayNumber

--- 7Kyu-Kata/findTheStrayNumber.js
+++ 7Kyu-Kata/findTheStrayNumber.js
@@ -10,8 +10,7 @@
 //Examples: see above
 //Pseudocode:
         //sort the array from least to greatest.
-        //Declare a variable and call it "minorityNum"
-        //if element at index 0 is the same as element as index 1, then minorityNum += that index[array.length]. Otherwise, return element at index 0
+        //if element at index 0 is the same as element as index 1 (meaning the stray number is NOT located at index 0 and 1), then return index[array.length-1]. Otherwise, return element at index 0
 //My Solution:
 function stray(numbers) {
     numbers.sort((a,b)=>a-b)
--- Codewars Practice.js
+++ Codewars Practice.js
@@ -4468,12 +4468,11 @@ getFetch()
 //Examples: see above
 //Pseudocode:
         //sort the array from least to greatest.
-        //Declare a variable and call it "minorityNum"
-        //if element at index 0 is the same as element as index 1, then minorityNum += that index[array.length]. Otherwise, return element at index 0
+        //if element at index 0 is the same as element as index 1 (meaning the stray number is NOT located at index 0 and 1), then return index[array.length-1]. Otherwise, return element at index 0
 //My Solution:
-// function stray(numbers) {
-//     numbers.sort((a,b)=>a-b)
-//     return Number(numbers[0]) === Number(numbers[1]) ? Number(numbers[numbers.length-1]) : Number(numbers[0])
-//   }
-// //Test case:
-// console.log(stray([1, 1, 2,1,1,1]))
+function stray(numbers) {
+    numbers.sort((a,b)=>a-b)
+    return Number(numbers[0]) === Number(numbers[1]) ? Number(numbers[numbers.length-1]) : Number(numbers[0])
+  }
+//Test case:
+console.log(stray([1, 1, 2,1,1,1]))
