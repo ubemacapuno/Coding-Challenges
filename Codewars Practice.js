f0f9050eadada4be8fad10d038a63102472e5ed4
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-08-15 06:31:39 -0700

    Added 8Kyu problem and solutions

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -4746,3 +4746,28 @@ getFetch()
 //     }
 //     return [a,b]
 // }
+
+// // 8 kyu Reverse List Order
+// // In this kata you will create a function that takes in a list and returns a list with the reverse order.
+// //Parameter: given array
+// //Return: return the array in reverse
+// //Example:
+// // * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// // * [9, 2, 0, 7]  -> [7, 0, 2, 9]
+// //Pseudocode:
+// //My solution:
+// function reverseList(list) {
+//     return list.reverse()
+// }
+// // //Refactored to arrow function:
+// const reverseList = list => list.reverse()
+// //Solution without using reverse() array method:
+// //Declare an empty array called returnArray
+// //Iterate through 'list' array using a loop, and use unshift() method to add to the returnArray
+// function reverseList(list) {
+//     let returnArray = []
+//     list.forEach(element => returnArray.unshift(element))
+//     return returnArray
+// }
+// //Test Case:
+// console.log(reverseList([1, 2, 3, 4]))
