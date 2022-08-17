47d5516ea6a688614e73d86ff575410923cb7ad9
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-08-17 10:37:41 -0700

    Added 8Kyu and 7Kyu problems and solutions

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -4771,3 +4771,62 @@ getFetch()
 // }
 // //Test Case:
 // console.log(reverseList([1, 2, 3, 4]))
+
+// 8Kyu Parse nice int from char problem 
+// Write a program that returns the girl's age (0-9) as an integer.
+// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+//Parameters: String
+//Return: Return the first character of the string as a number
+//Example: "1 year old"  returns 1 with a type of number
+//Pseudocode: Return the first index aka [0] of the string, but convert it to a number:
+//My solution:
+// function getAge(inputString){
+//     return inputString[0]*1
+//     }
+// //Refactored:
+// const getAge = inputString => inputString[0]*1
+
+// // 8 kyu Super Duper Easy
+// //Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+// function problem(x){
+//     return typeof x === 'number' ? x*50 + 6 : "Error"
+//   }
+// // refactored:
+// const problem = x => typeof x === 'number' ? x*50 + 6 : "Error"
+
+// 7 kyu Collatz Conjecture Length
+// The Collatz Conjecture states that for any natural number n, if n is even, divide it by 2. If n is odd, multiply it by 3 and add 1. If you repeat the process continuously for n, n will eventually reach 1.
+// For example, if n = 20, the resulting sequence will be:
+// [ 20, 10, 5, 16, 8, 4, 2, 1 ]
+// Write a program that will output the length of the Collatz Conjecture for any given n.
+// In the example above, the output would be 8.
+//Parameter: given number n
+//Return: Length of the collatz conjecture array
+//Example: 
+    // 20 -> 8
+    // 15 -> 18
+//Pseudocode: 
+    //Declare a return array called collatz array, which will equal [n], and we will push to this array
+    //Write out the collatz conjecture:
+        //have condition:
+            //if n is even: divide by 2. Push to the array
+            //If odd, multiply by 3 and add 1. Push to the array
+
+    //return the length of the array at the end
+// function collatz(n) {
+//     let collatzArray = []
+//     while (n != 1){
+//         if(n%2 === 0){
+//             collatzArray.push(n/2)
+//             // console.log(collatzArray)
+//             n = n/2
+//         }else{
+//             collatzArray.push(3 * n + 1)
+//             // console.log(collatzArray)
+//             n = 3 * n + 1
+//         }
+//     }
+//     return collatzArray.length + 1
+// }
+// //Test Case
+// console.log(collatz(73567465519280238573)) //561
