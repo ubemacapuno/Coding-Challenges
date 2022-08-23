02300973e5352fc8b5d2bc1bbb46f656445d1ddb
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-08-23 11:27:26 -0700

    Added 7Kyu problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -4995,9 +4995,29 @@ getFetch()
 // }
 // console.log(dontGiveMeFive(-90000000000000, 900000000000000))
 
-var merge = function(nums1, m, nums2, n) {
-   nums1.concat(nums2).sort((a,b)=>a-b)
-   console.log(nums1)
- };
-
- console.log(merge([1,2,3,0,0,0],3,[2,5,6],3))
+//7 kyu The Coupon Code
+// Story
+// Your online store likes to give out coupons for special occasions. Some customers try to cheat the system by entering invalid codes or using expired coupons.
+// Task
+// Your mission:
+// Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+// A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+// Examples:
+// checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+// checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+//P: enteredCode, correctCode, currentDate, expirationDate
+//R: true or false
+//E: 
+        // checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+        // checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+        // checkCoupon('123','123','September 5, 2014','October 1, 2014'), true)
+        // checkCoupon('123a','123','September 5, 2014','October 1, 2014'), false)
+//Pseudocode:
+//Conditional statements - if enteredCode === correctCode and currentDate <= expirationDate, return TRUE else false
+//To convert the date from string to date so that we can use comparison operators, use Date.parse(dateStringHere)
+//My solution:
+// function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+//     return enteredCode === correctCode && Date.parse(currentDate) <= Date.parse(expirationDate)
+// }
+// //Arrow fxn:
+// const checkCoupon = (enteredCode, correctCode, currentDate, expirationDate) => enteredCode === correctCode && Date.parse(currentDate) <= Date.parse(expirationDate)
