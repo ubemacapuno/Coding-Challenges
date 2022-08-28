ce86b6fef031e71203d91f1d15194d70af268bc8
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-08-27 20:19:27 -0700

    Added 7Kyu problem associated with strings

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -5135,3 +5135,24 @@ getFetch()
 // console.log(isPalindrome(5454))
 // //Arrow syntax:
 // const isPalindrome = line => String(line) === String(line).split('').reverse().join('')
+
+// //7 kyu Convert an array of strings to array of numbers
+// // Oh no!
+// // Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
+// // You need to cast the whole array to the correct type.
+// // Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+// // ie:["1", "2", "3"] to [1, 2, 3]
+// // Note that you can receive floats as well.
+// //P: array of STRINGS
+// //R: Same array but with NUMBERS
+// //E: see above
+// //Pseudocode:
+//         //map an array, but use Number primitive
+// //my solution:
+// function toNumberArray(stringarray){
+//     return stringarray.map(e => Number(e))
+// }
+// //Arrow Syntax:
+// const toNumberArray = stringarray => stringarray.map(e => Number(e))
+// //Test Case:
+// console.log(toNumberArray(["1", "2", "3"]))
