929176a3f9534c2f6fbc802b45bc8ca70ae19e8b
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-08-31 21:17:48 -0700

    Added 8Kyu problem and solution, working on 7Kyu

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -5271,3 +5271,58 @@ getFetch()
 //     }
 // //Arrow solution:
 // const bonusTime = (salary,bonus) => bonus ? `£${salary*10}` : `£${salary}`
+
+// // 8 kyu 5 without numbers !!
+// // Write a function that always returns 5
+// // Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+// // Good luck :)
+// //P: None
+// //R: 5 but without using characters 0123456789*+-/
+// //E: No example just return 5!!!!
+// //Pseudocode: Use length property to return a 5
+// //My solution:
+// const unusualFive = () => [...'lmfao'].length;
+
+// Imagine you are trying to roll a ball a certain distance down a road. The ball will have a starting speed that slowly degrades due to friction and cracks in the road. Every time the ball rolls a distance equal to its speed or rolls over a crack, its speed decreases by 1. Given a speed of s which the ball starts rolling, and a roadmap r of the street represented by a string, return whether or not the ball will be able to make it past the end of the road (True or False).
+// NOTES
+// A ball with 0 speed is motionless.
+// If a ball happens to roll over a crack, the speed decrease must only take effect after the ball has rolled a distance equal to its speed. For example, if the speed was 10, but the ball hit 2 cracks before rolling 10 distance, the ball's speed should stay at 10 until it has reached 10 distance, in which the speed should decrease to 7 (-2 from cracks and -1 due to friction).
+// On the roadmap, "_" represents flat ground and "x" represents a crack
+// The length of the roadmap will be equal to the distance to the ball's final destination
+// The ball must fall off of the edge of the road for the code to be considered valid.
+//P: Speed represented by s and a roadmap, represented by r
+//R: true or false (true if the ball will make it past the end of the road, false if not)
+//E: 
+    // A speed of 100, and a roadmap of '_' should return True because the ball would be moving too quickly for friction to be applied and there are no cracks on the road.
+    // A speed of 1, and a roadmap of '___________' should return False because friction would stop the ball after 1 distance
+//Pseudocode:
+    //equal to or less than 0 returns false
+    //
+function ballTest(s, r) {
+    let totalLength = r.length
+    let totalCrack = 0
+    let totalFlat = 0
+    let initialSpeed = s
+    for(let i=0; i<r.length; i++){
+        if(r[i]==="x"){
+            totalCrack++
+        }else{
+            totalFlat++
+        }
+    }
+    console.log(`Initial Speed ${s}`)
+    console.log(`totalLength ${totalLength}`)
+    console.log(`totalCrack ${totalCrack}`)
+    console.log(`totalFlat ${totalFlat}`)
+
+    if(s<=0 || (s - totalCrack + initialSpeed) > 0){
+        return false
+    }else if(totalFlat === 0 && totalCrack > s || totalCrack === 0 && totalFlat > s){
+        return false
+    }else{
+        true
+    }   
+}
+//Test case:
+console.log(ballTest(24, "xxxxxxxxxx_____x___xx__xx____________x__________x_")) //true
+
