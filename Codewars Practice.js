6e53d65325450d647450bd0665fb6b882f892ebc
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-09-01 23:23:43 -0700

    8Kyu string problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -5298,31 +5298,60 @@ getFetch()
 //Pseudocode:
     //equal to or less than 0 returns false
     //
-function ballTest(s, r) {
-    let totalLength = r.length
-    let totalCrack = 0
-    let totalFlat = 0
-    let initialSpeed = s
-    for(let i=0; i<r.length; i++){
-        if(r[i]==="x"){
-            totalCrack++
-        }else{
-            totalFlat++
-        }
-    }
-    console.log(`Initial Speed ${s}`)
-    console.log(`totalLength ${totalLength}`)
-    console.log(`totalCrack ${totalCrack}`)
-    console.log(`totalFlat ${totalFlat}`)
+// function ballTest(s, r) {
+//     let totalLength = r.length
+//     let totalCrack = 0
+//     let totalFlat = 0
+//     let initialSpeed = s
+//     for(let i=0; i<r.length; i++){
+//         if(r[i]==="x"){
+//             totalCrack++
+//         }else{
+//             totalFlat++
+//         }
+//     }
+//     console.log(`Initial Speed ${s}`)
+//     console.log(`totalLength ${totalLength}`)
+//     console.log(`totalCrack ${totalCrack}`)
+//     console.log(`totalFlat ${totalFlat}`)
 
-    if(s<=0 || (s - totalCrack + initialSpeed) > 0){
-        return false
-    }else if(totalFlat === 0 && totalCrack > s || totalCrack === 0 && totalFlat > s){
-        return false
-    }else{
-        true
-    }   
-}
-//Test case:
-console.log(ballTest(24, "xxxxxxxxxx_____x___xx__xx____________x__________x_")) //true
+//     if(s<=0 || (s - totalCrack + initialSpeed) > 0){
+//         return false
+//     }else if(totalFlat === 0 && totalCrack > s || totalCrack === 0 && totalFlat > s){
+//         return false
+//     }else{
+//         true
+//     }   
+// }
+// //Test case:
+// console.log(ballTest(24, "xxxxxxxxxx_____x___xx__xx____________x__________x_")) //true
 
+// // 8 kyu Contamination #1 -String-
+// // An AI has infected a text with a character!!
+// // This text is now fully mutated to this character.
+// // If the text or the character are empty, return an empty string.
+// // There will never be a case when both are empty as nothing is going on!!
+// // Note: The character is a string of length 1 or an empty string.
+// // Example
+// // text before = "abc"
+// // character   = "z"
+// // text after  = "zzz"
+// //P: String and single character
+// //R: Return the string but replaced with the single character. However return empty string if string or character are empty
+// //E:
+//         // Test.assertEquals(contamination("abc","z"), "zzz")
+//         // Test.assertEquals(contamination("","z"), "")
+//         // Test.assertEquals(contamination("abc",""), "")
+//         // Test.assertEquals(contamination("_3ebzgh4","&"), "&&&&&&&&")
+//         // Test.assertEquals(contamination("//case"," "), "      ")
+// //Pseudocode:
+//     //Use a for loop. up to the length of the string, add "char" to a return string
+// //My solution:
+// function contamination(text, char){
+//     let returnString = ""
+//     for(let i=0; i<text.length; i++){
+//         returnString += char
+//     }
+//     return returnString
+//   }
+// console.log(contamination("ffff","g"))
