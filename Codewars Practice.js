35a958e411e68866407a02335afe009c87a2fffa
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-09-09 14:56:40 -0700

    Added 7Kyu array problem and solutions

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -5594,3 +5594,28 @@ getFetch()
 // }
 // //Test case:
 // console.log(sumOfMinimums([ [ 1, 2, 3, 4, 5 ], [ 5, 6, 7, 8, 9 ], [ 20, 21, 34, 56, 100 ]]))
+
+// 7 kyu Spacify
+// Modify the spacify function so that it returns the given string with spaces inserted between each character.
+// spacify("hello world") // returns "h e l l o   w o r l d"
+//P: string
+//R: same string, but with spaces inserted between each character
+//E: (spacify('hello world'),'h e l l o   w o r l d')
+//Pseudocode:
+    //Use a for loop and iterate through the string
+    //through each iteration, add " " after each character and return the string
+//My Solution:
+// function spacify(str) {
+//     let strArray = []
+//     for (let i=0; i<str.length-1; i++){
+//         strArray.push(`${str[i]} `)
+//     }
+//     return strArray.join('') + str[str.length-1]
+// }
+// //Solution using Reduce - suggestion by Eric
+// function spacify(str) {
+//     let reducedStr = [...str].reduce((accumulator,currentValue) => accumulator + `${currentValue} `,"")
+//     return reducedStr.slice(0,-1)
+// }
+// //Test Case:
+// console.log(spacify("Hello World"))
