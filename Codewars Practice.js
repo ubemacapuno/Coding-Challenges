e9e433f9648879dac64dc695671f891189ccce64
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-09-10 20:07:17 -0700

    Added 7Kyu array problem

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -5594,3 +5594,30 @@ getFetch()
 // }
 // //Test case:
 // console.log(sumOfMinimums([ [ 1, 2, 3, 4, 5 ], [ 5, 6, 7, 8, 9 ], [ 20, 21, 34, 56, 100 ]]))
+
+// 7 kyu Counting Array Elements
+// Write a function that takes an array and counts the number of each unique element present.
+// count(['james', 'james', 'john']) 
+// #=> { 'james': 2, 'john': 1}
+//P: given an array
+//Returns object with the number of each unique element present
+//E: See above
+//Pseudocode:
+    //Declare an empty object that we will add to and return at the end
+    //iterate throuugh the array and add to the object:
+        //If the element has not been seen, make a key and assign value of 1
+        //If the element has been seen, increase the value ++
+    //Return the object
+//My solution:
+
+// function count(array){
+//     const map = {}
+//     array.forEach(function(value, key) {
+//       if (!map[value]) {
+//         map[value] = 1
+//       } else {
+//         map[value] ++
+//       }
+//     })
+//     return map;
+//   }
