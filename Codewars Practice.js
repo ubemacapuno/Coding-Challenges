cddae57a58ed190534a0413bd18611176e7f91f2
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-09-20 14:26:51 -0700

    Added 7Kyu problem and solution

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -5990,3 +5990,27 @@ getFetch()
 //     }
 // //Test:
 // console.log(firstNonConsecutive([1,2,3,7))
+
+// // 7 kyu Add property to every object in array
+// // Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
+// // After adding the property the result should be:
+// // var questions = [{
+// //     question: "What's the currency of the USA?",
+// //     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+// //     corAnswer: 0,
+// //     usersAnswer: null
+// // }, {
+// //     question: "Where was the American Declaration of Independence signed?",
+// //     choices: ["Philadelphia", "At the bottom", "Frankie's pub", "China"],
+// //     corAnswer: 0,
+// //     usersAnswer: null
+// // }];
+// //P: Given an array of objects
+// //R: Return the array of objects after having added usersAnswer: null to each object
+// //E: See above example
+// //Pseudocode:
+//     //use map to apply userAnswer = null to each object in the original array
+// //My solution:
+// questions.map(function(element){
+//     element.usersAnswer = null;
+//   });
