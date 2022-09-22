bf609740e21cb170b9cf7f4c5e0e0131c52a5bca
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-09-22 09:25:41 -0700

    Added 7Kyu array problem

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -6037,3 +6037,12 @@ getFetch()
             //Return the last element.
 //My solution:
 // const likeOrDislike = buttons => buttons.reduce((acc,c) => c == acc ? Nothing : c,Nothing)
+
+// // 7 kyu Sort arrays - 1
+// // Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+// //P: unsorted array
+// //R: sorted array (lexicographical order)
+// //E: (sortme(['one', 'two', 'three' ]) returns ["one", "three", "two"])
+// //Pseudocode: use sort method on the given array
+// //My solution:
+// const sortme = names => names.sort()
