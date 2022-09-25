6c7d98ff543537376e56af345ce2c9982806493d
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-09-25 12:09:45 -0700

    Added 8Kyu array problem

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -6046,3 +6046,18 @@ getFetch()
 // //Pseudocode: use sort method on the given array
 // //My solution:
 // const sortme = names => names.sort()
+
+// // 8 kyu Basic Training: Add item to an Array
+// // Add the value "codewars" to the websites array.
+// // After your code executes the websites array should == ["codewars"]
+// // The websites array has already been defined for you using the following code:
+// // var websites = [];
+// //P: Given empty array called 'websites'
+// //R: your code executes the websites array should == ["codewars"]
+// //E: websites == ["codewars"]
+// //Pseudocode:
+//     //use push method to push string 'codewars' to the empty array
+//     //use bracket of element 0 to equal string codewars
+// //Solutions:
+// websites.push("codewars")
+// websites[0]="codewars"
