39a205a47e6485937204b1b2e7cc90161d854faf
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-09-27 22:15:50 -0700

    Added 6Kyu string problem

--- Codewars Practice.js
+++ Codewars Practice.js
@@ -6061,3 +6061,32 @@ getFetch()
 // //Solutions:
 // websites.push("codewars")
 // websites[0]="codewars"
+
+// // 6 kyu Break camelCase
+// // Complete the solution so that the function will break up camel casing, using a space between words.
+// // Example
+// // "camelCasing"  =>  "camel Casing"
+// // "identifier"   =>  "identifier"
+// // ""             =>  ""
+// //P: Given a string
+// //R: Break up the camel casing, placing a string between words
+// //E: See examples above
+// //Pseudocode:
+//     //Have an empty string that we will add to and return at the end.
+//     //Use a loop which will iterate through the string. 
+//     //If a capital letter is found, add a space plus the letter to the returnString.
+//     //Otherwise just add the character to the returnString
+// // my solution:
+// function solution(string) {
+//     let returnString = ""
+//     for(let i=0; i<string.length;i++){
+//         if(string[i] === string[i].toUpperCase()){
+//             returnString += ` ${string[i]}`
+//         }else{
+//             returnString += `${string[i]}`
+//         }
+//     }
+//     return returnString
+// }
+// //Test Case:
+// console.log(solution(''))
