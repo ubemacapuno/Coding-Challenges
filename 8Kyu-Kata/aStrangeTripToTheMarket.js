06cbb9c3599caabac7e66779d2f75b78bc819ac9
Fast DMS Corey <fastdmscorey@Coreys-MacBook-Air.local>
2023-01-31 09:05:49 -0700

    Added 8Kyu

---
+++ 8Kyu-Kata/aStrangeTripToTheMarket.js
@@ -0,0 +1,17 @@
+// 8 kyu A Strange Trip to the Market
+// Since Nessie is a master of disguise, the only way accurately tell is to look for the phrase "tree fiddy". Since you are tired of being grifted by this monster, the time has come to code a solution for finding The Loch Ness Monster. Note that the phrase can also be written as "3.50" or "three fifty". Your function should return true if you're talking with The Loch Ness Moster, false otherwise.
+//P: string
+//R: true or false IF string contains the following:
+//tree fiddy
+//3.50
+//three fifty
+//E: "Your girlscout cookies are ready to ship. Your total comes to tree fiddy" returns true
+//Psuedocode:
+//place strings inside of an array, and use some and includes methods to check if the string has those substrings in the array
+// solution:
+function isLockNessMonster(s) {
+  const tree = ["tree fiddy", "3.50", "three fifty"];
+  console.log(tree.some((e) => s.includes(e)));
+}
+//Test case:
+console.log(isLockNessMonster("tree fiddy"));
