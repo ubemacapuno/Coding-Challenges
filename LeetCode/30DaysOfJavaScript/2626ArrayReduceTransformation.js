b334c5380550c4fb1df33f1748d91a5f63f59445
Fast DMS Corey <corey.damocles@fastdms.com>
2024-03-31 17:00:18 -0700

    Add JS leetcode

---
+++ LeetCode/30DaysOfJavaScript/2626ArrayReduceTransformation.js
@@ -0,0 +1,58 @@
+/**
+P: integer array 'nums', a reducer function 'fn', initial value 'init'
+R: for empty arrays, return init. Otherwise return the accumulation 
+Example 1:
+
+Input: 
+nums = [1,2,3,4]
+fn = function sum(accum, curr) { return accum + curr; }
+init = 0
+Output: 10
+Explanation:
+initially, the value is init=0.
+(0) + nums[0] = 1
+(1) + nums[1] = 3
+(3) + nums[2] = 6
+(6) + nums[3] = 10
+The final answer is 10.
+Example 2:
+
+Input: 
+nums = [1,2,3,4]
+fn = function sum(accum, curr) { return accum + curr * curr; }
+init = 100
+Output: 130
+Explanation:
+initially, the value is init=100.
+(100) + nums[0] * nums[0] = 101
+(101) + nums[1] * nums[1] = 105
+(105) + nums[2] * nums[2] = 114
+(114) + nums[3] * nums[3] = 130
+The final answer is 130.
+Example 3:
+
+Input: 
+nums = []
+fn = function sum(accum, curr) { return 0; }
+init = 25
+Output: 25
+Explanation: For empty arrays, the answer is always init.
+
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function (nums, fn, init) {
+  let res = init;
+
+  // iterate through each element in nums
+  for (let i = 0; i < nums.length; i++) {
+    // apply 'fn' to the current result ('res') and the current element
+    // update 'res' with the new value returned from 'fn'
+    res = fn(res, nums[i]);
+  }
+
+  // return res
+  return res;
+};
